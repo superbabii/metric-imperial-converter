4b53bcabdba9d359e6c673a324549a8ed26af6c5
superbabii <nzrbabii@gmail.com>
2024-11-02 16:49:16 +0200

    f

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,7 +6,7 @@ chai.use(chaiHttp);
 
 describe('Functional Tests', () => {
   it('Convert a valid input (10L)', (done) => {
-    chai.request(app)
+    chai.request('https://metric-imperial-converter-chi.vercel.app')
       .get('/api/convert')
       .query({ input: '10L' })
       .end((err, res) => {
@@ -20,7 +20,7 @@ describe('Functional Tests', () => {
   });
 
   it('Convert an invalid input unit (32g)', (done) => {
-    chai.request(app)
+    chai.request('https://metric-imperial-converter-chi.vercel.app')
       .get('/api/convert')
       .query({ input: '32g' })
       .end((err, res) => {
@@ -31,7 +31,7 @@ describe('Functional Tests', () => {
   });
 
   it('Convert an invalid number (3/7.2/4kg)', (done) => {
-    chai.request(app)
+    chai.request('https://metric-imperial-converter-chi.vercel.app')
       .get('/api/convert')
       .query({ input: '3/7.2/4kg' })
       .end((err, res) => {
@@ -42,7 +42,7 @@ describe('Functional Tests', () => {
   });
 
   it('Convert an invalid number and unit (3/7.2/4kilomegagram)', (done) => {
-    chai.request(app)
+    chai.request('https://metric-imperial-converter-chi.vercel.app')
       .get('/api/convert')
       .query({ input: '3/7.2/4kilomegagram' })
       .end((err, res) => {
@@ -53,7 +53,7 @@ describe('Functional Tests', () => {
   });
 
   it('Convert with no number (kg)', (done) => {
-    chai.request(app)
+    chai.request('https://metric-imperial-converter-chi.vercel.app')
       .get('/api/convert')
       .query({ input: 'kg' })
       .end((err, res) => {
