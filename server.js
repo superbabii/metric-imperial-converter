4297426001bcb42c10ba5d3b87b06de50f7f7e70
superbabii <nzrbabii@gmail.com>
2024-11-02 16:41:49 +0200

    f

--- server.js
+++ server.js
@@ -5,11 +5,4 @@ const app = express();
 
 app.use('/api', apiRoutes);
 
-if (process.env.NODE_ENV !== 'test') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-
 export default app;
